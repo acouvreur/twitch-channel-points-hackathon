146dea4795993d824e136423fb9e034bab122cee
Thomas Moreau <thomas.moreau.f@gmail.com>
2021-03-01 01:14:55 +0100

    made POLLIN_INTERVAL configurable

--- polling/index.js
+++ polling/index.js
@@ -2,7 +2,9 @@ const sleep = require('util').promisify(setTimeout);
 
 const channelInfoPolling = require('./channel-info.polling');
 
-const POLLING_INTERVAL = 5000;
+const POLLING_INTERVAL = process.env.POLLING_INTERVAL
+  ? parseInt(process.env.POLLING_INTERVAL, 10) : 5000;
+
 let isPolling = false;
 
 const doPoll = async () => {
