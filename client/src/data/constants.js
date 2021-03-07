8a9f30981f7d83da31cf4650ffdfc3b45edc92c3
Florian Maunier <fmauneko@dissidence.ovh>
2021-03-07 20:34:55 +0100

    Describe MIDI message types
    Co-authored-by: grichka <grichka@users.noreply.github.com>

--- client/src/data/constants.js
+++ client/src/data/constants.js
@@ -1,3 +1,111 @@
+/**
+ * @type {Object.<string, {displayName: string, params: string[]}}
+ */
+const MIDI_MESSAGE_TYPES = {
+  noteon: {
+    displayName: 'Note On',
+    params: [
+      'note',
+      'velocity',
+      'channel',
+    ],
+  },
+  noteoff: {
+    displayName: 'Note Off',
+    params: [
+      'note',
+      'velocity',
+      'channel',
+    ],
+  },
+  'poly aftertouch': {
+    displayName: 'Polyphonic Key Pressure (Aftertouch)',
+    params: [
+      'note',
+      'velocity',
+      'channel',
+    ],
+  },
+  cc: {
+    displayName: 'Control Change',
+    params: [
+      'controller',
+      'value',
+      'channel',
+    ],
+  },
+  program: {
+    displayName: 'Program Change',
+    params: [
+      'number',
+      'channel',
+    ],
+  },
+  'channel aftertouch': {
+    displayName: 'Channel Pressure (Aftertouch)',
+    params: [
+      'pressure',
+      'channel',
+    ],
+  },
+  pitch: {
+    displayName: 'Pitch Bend Change',
+    params: [
+      'value',
+      'channel',
+    ],
+  },
+  position: {
+    displayName: 'Song Position Pointer',
+    params: [
+      'value',
+    ],
+  },
+  mtc: {
+    displayName: 'MIDI Time Code Quarter Frame',
+    params: [
+      'type',
+      'value',
+    ],
+  },
+  select: {
+    displayName: 'Song Select',
+    params: [
+      'song',
+    ],
+  },
+  clock: {
+    displayName: 'Timing Clock',
+    params: [],
+  },
+  start: {
+    displayName: 'Start',
+    params: [],
+  },
+  continue: {
+    displayName: 'Continue',
+    params: [],
+  },
+  stop: {
+    displayName: 'Stop',
+    params: [],
+  },
+  activesense: {
+    displayName: 'Active Sensing',
+    params: [],
+  },
+  reset: {
+    displayName: 'Reset',
+    params: [],
+  },
+  sysex: {
+    displayName: 'System Exclusive',
+    params: [
+      'byteArray',
+    ],
+  },
+};
+
 const MINECRAFT_POTION_EFFECTS = [
   'SPEED',
   'SLOWNESS',
@@ -34,6 +142,6 @@ const MINECRAFT_POTION_EFFECTS = [
 ];
 
 export {
-  // eslint-disable-next-line import/prefer-default-export
+  MIDI_MESSAGE_TYPES,
   MINECRAFT_POTION_EFFECTS,
 };
