716f9eddfccfce2094e61288608d1b5ae1e1beef
Thyvador <alex.hiltcher@gmail.com>
2021-03-07 12:41:53 +0100

    add cors middleware

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const routes = require('./routes');
 const { handleHttpErrorResponse } = require('../middleware/handleHttpErrorResponse.middleware');
 
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json({ limit: '5mb', extended: true }));
 
