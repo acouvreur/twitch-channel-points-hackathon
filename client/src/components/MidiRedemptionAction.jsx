7fe53b2bf68ceaf5cb8f569b5db3742839123507
Florian Maunier <fmauneko@dissidence.ovh>
2021-03-08 19:41:14 +0100

    Semi-working midi action form

--- client/src/components/MidiRedemptionAction.jsx
+++ client/src/components/MidiRedemptionAction.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import { MenuItem, Select } from '@material-ui/core';
+import {
+  Card,
+  CardContent,
+  MenuItem,
+  Select,
+  TextField,
+} from '@material-ui/core';
 import { MIDI_MESSAGE_TYPES } from '../data/constants';
 
 const MidiRedemptionAction = ({ action, onChange }) => {
-  const onSelectMessageType = (event, value) => {
+  const onSelectMessageType = (order) => (event, value) => {
     onChange({ ...action, params: { ...action.params, effect: value } });
   };
 
   return (
     <>
-      <Select
-        labelId="redemption-type-label"
-        id="redemption-type-select"
-        value={action?.params?.effect || Object.keys(MIDI_MESSAGE_TYPES)[0]}
-        onChange={onSelectMessageType}
-      >
-        {Object.entries(MIDI_MESSAGE_TYPES).map(([type, config]) => (
-          <MenuItem key={type} value={type}>{config.displayName}</MenuItem>
-        ))}
-      </Select>
+      {
+        action?.params?.outputs.map((output) => (
+          <Card variant="outlined" key={output.order}>
+            <CardContent>
+              <Select
+                value={output.type}
+                onChange={onSelectMessageType(output.order)}
+                fullWidth
+              >
+                {
+                  Object.entries(MIDI_MESSAGE_TYPES).map(([type, config]) => (
+                    <MenuItem key={type} value={type}>{config.displayName}</MenuItem>
+                  ))
+                }
+              </Select>
+              {
+                MIDI_MESSAGE_TYPES[output.type].params.map((param) => (
+                  <TextField
+                    key={param}
+                    label={param.charAt(0).toUpperCase() + param.slice(1)}
+                    variant="outlined"
+                    value={output.value[param]}
+                    margin="normal"
+                    fullWidth
+                  />
+                ))
+              }
+            </CardContent>
+          </Card>
+        ))
+      }
     </>
   );
 };
