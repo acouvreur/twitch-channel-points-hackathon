4ccdd35e2a054b7bd76e9aefda568b3558711b73
Thomas Moreau <thomas.moreau.f@gmail.com>
2021-03-10 19:15:35 +0100

    Handle errors in controllers, and clean up

--- authentication/twitch-auth.controller.js
+++ authentication/twitch-auth.controller.js
@@ -6,14 +6,18 @@ router.get('/', async (req, res) => {
   res.redirect(twitchAuthService.TWITCH_AUTHORIZE_URL);
 });
 
-router.get('/callback', async (req, res) => {
+router.get('/callback', async (req, res, next) => {
   console.log('[HTTP] GET /auth/callback');
   console.log('[LOG] generating token.json (DO NOT SHARE IT)...');
 
-  twitchAuthService.generateTokenFile(req.query.code);
+  try {
+    await twitchAuthService.generateTokenFile(req.query.code);
+  } catch (err) {
+    return next(err);
+  }
 
   console.log('[LOG] Success!');
-  res.status(200).send('<h1>Success!</h1><p>You can close this tab now.</p>');
+  return res.status(200).send('<h1>Success!</h1><p>You can close this tab now.</p>');
 });
 
 module.exports = router;
