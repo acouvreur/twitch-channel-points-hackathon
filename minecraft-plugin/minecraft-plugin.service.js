92f89e73080b13492b0e3890aee02b29fb05db8f
Thyvador <alex.hiltcher@gmail.com>
2021-03-11 21:58:23 +0100

    update minecraft controller

--- minecraft-plugin/minecraft-plugin.service.js
+++ minecraft-plugin/minecraft-plugin.service.js
@@ -3,11 +3,7 @@ const { MinecraftError } = require('../errors');
 
 const minecraftPluginServerUrl = process.env.PLUGIN_MINECRAFT_SERVER_URL;
 
-/**
- * @param {import('twitch-pubsub-client').PubSubRedemptionMessage} redemptionMessage
- * @param {object} params
- */
-const applyEffect = async (redemptionMessage, params) => {
+const applyPotionEffect = async (redemptionMessage, params) => {
   console.log(`[Minecraft Plugin] Applying effects for redemption [${redemptionMessage.rewardName}] (${redemptionMessage.rewardCost} channel points) redeemed by [${redemptionMessage.userDisplayName}]`);
 
   console.log('[Minecraft Plugin] Applying Minecraft potion effect');
@@ -27,6 +23,41 @@ const applyEffect = async (redemptionMessage, params) => {
       message = 'Could not contact Minecraft client.';
     }
     throw new MinecraftError(message);
+  }
+};
+
+const applyWeatherEffect = async (redemptionMessage, params) => {
+  console.log(`[Minecraft Plugin] Applying effects for redemption [${redemptionMessage.rewardName}] (${redemptionMessage.rewardCost} channel points) redeemed by [${redemptionMessage.userDisplayName}]`);
+
+  console.log('[Minecraft Plugin] Applying Minecraft weather effect');
+  try {
+    await got.post(`${minecraftPluginServerUrl}/weather`, {
+      json: {
+        redeemedBy: redemptionMessage.userDisplayName,
+        rewardCost: redemptionMessage.rewardCost,
+        ...params,
+      },
+      responseType: 'json',
+    });
+  } catch (err) {
+    console.error(`[Minecraft Plugin] ${err.message}`);
+    let { message } = err;
+    if (err.code === 'ECONNREFUSED') {
+      message = 'Could not contact Minecraft client.';
+    }
+    throw new MinecraftError(message);
+  }
+};
+
+/**
+ * @param {import('twitch-pubsub-client').PubSubRedemptionMessage} redemptionMessage
+ * @param {object} params
+ */
+const applyEffect = async (redemptionMessage, params) => {
+  if (params.type === 'potion') {
+    applyPotionEffect(redemptionMessage, params);
+  } else if (params.type === 'weather') {
+    applyWeatherEffect(redemptionMessage, params);
   }
 };
 
